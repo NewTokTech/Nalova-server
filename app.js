ec56753c4e3a20d917fead59093dd4fd77608063
shibil <shibi2772@gmail.com>
2023-11-16 20:58:38 +0530

    shibil

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: ['https://nalova-react.vercel.app/', 'http://localhost:3000'],
+  origin: ['https://nalova-react.vercel.app', 'http://localhost:3000'],
   credentials: true
 }));
 
